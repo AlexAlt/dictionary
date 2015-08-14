c220a13287bca403734709c13c80c8951a31e88b
AlexAlt <alexandra.m.altieri@gmail.com>
2015-08-14 10:37:50 -0700

    Pass spec to return language

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,10 +1,10 @@
 class Definition 
-  
-  attr_reader(:part_of_speech, :language, :definition)
+
+  attr_reader(:part_of_speech, :language, :description)
 
   define_method(:initialize) do |attributes|
     @part_of_speech = attributes.fetch(:part_of_speech)
     @language = attributes.fetch(:language)
-    @definition = attributes.fetch(:definition)
+    @definition = attributes.fetch(:description)
   end
 end
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -3,9 +3,16 @@ require('definition')
 
 describe(Definition) do
   describe('#part_of_speech') do
-    it('returns the part of speech of the definition') do
-      test_definition = Definition.new(:part_of_speech => "adjective", :language => "English", :definition => "of a color red")
+    it('returns the part of speech') do
+      test_definition = Definition.new(:part_of_speech => "adjective", :language => "English", :description => "of a color red")
       expect(test_definition.part_of_speech()).to(eq("adjective"))
+    end
+  end
+
+   describe('#language') do
+    it('returns the language') do
+      test_definition = Definition.new(:part_of_speech => "adjective", :language => "English", :description => "of a color red")
+      expect(test_definition.language()).to(eq("English"))
     end
   end
 end
