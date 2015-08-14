a8c81ae96972b850bc2174329bc650b52363c948
AlexAlt <alexandra.m.altieri@gmail.com>
2015-08-14 12:09:42 -0700

    Add form to create a new word and add it to your dixtionary; add a page with list of all words

--- app.rb
+++ app.rb
@@ -4,3 +4,18 @@ require('./lib/definition')
 require('./lib/word')
 also_reload('lib/**/*.rb')
 
+get('/') do 
+  erb(:index)
+end
+
+post('/new_word') do
+  word = params.fetch('new_word')
+  new_word = Word.new(:word => word)
+  new_word.save()
+  redirect(:word_list)
+end
+
+get('/word_list') do
+  @all_words = Word.all()
+  erb(:word_list)
+end
