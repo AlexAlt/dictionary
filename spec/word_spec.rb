f0e31bb698e7d57a4ac34861da49469c4720984d
AlexAlt <alexandra.m.altieri@gmail.com>
2015-08-14 10:11:41 -0700

    Add Gemfile, and spec file. It will create specs for Word class

---
+++ spec/word_spec.rb
@@ -0,0 +1,32 @@
+require('word')
+require('rspec')
+
+describe(Word) do 
+  describe('#show') do
+    it('returns the word') do
+      test_word = Word.new('red')
+      expect(test_word.show()).to(eq('red'))
+   end
+  end
+
+  describe('.all') do
+    it('initially returns an empty array') do
+      expect(Word.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('saves a word to the word array') do
+      test_word = Word.new('red')
+      test_word.save()
+      expect(Word.all()).to(eq([test_word]))
+    end
+  end
+
+  describe('.clear') do
+    it('clears the array') do
+      Word.clear()
+      expect(Word.all()).to(eq([]))
+    end
+  end
+end
